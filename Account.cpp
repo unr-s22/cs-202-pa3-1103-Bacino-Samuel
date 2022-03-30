ed75154dd6201117606eb0184d3c5fc3f0e8f95d
Domminic Mayer <domminicmayer@gmail.com>
2022-03-29 18:46:06 -0700

    .

--- Account.cpp
+++ Account.cpp
@@ -19,38 +19,15 @@ void Account::makeWithdrawals (Money) {
 }
 
 std::ostream& operator<<(std::ostream &stream, const Account &acc){
-    if (Account::needsUpdate) {
-        int valC, valD;
-        valC = Account::initialBalance.getCents();
-        valD = Account::initialBalance.getDollars();
-        
-        if (Account::deposits.size() > 0) {
-            for (int i=0; i < Account::deposits.size(); i++) {
-                valC += Account::deposits[i].getCents();
-                valD += Account::deposits[i].getDollars();
-            }
-        }
-
-        if (Account::withdraws.size() > 0) {
-            for (int i=0; i < Account::withdraws.size(); i++) {
-                valC -= Account::withdraws[i].getCents();
-                valD -= Account::withdraws[i].getDollars();
-            }
-        }
-
-        Account::initialBalance.setCents(valC);
-        Account::initialBalance.setDollars(valD);
+    stream<< "Account Details\n--------------------------\nCurrent Balance:" << acc.initialBalance
+    << "\n--------------------------\nNumber of Deposits: " << acc.tD << "\n--------------------" << std::endl;
+    for (int i=0; i < acc.deposits.size(); i++) {
+        stream << "(" << i << ") " << acc.deposits[i] << std::endl;
     }
 
-    stream<< "Account Details\n--------------------------\nCurrent Balance:" << Account::initialBalance
-    << "\n--------------------------\nNumber of Deposits: " << Account::tD << "\n--------------------" << std::endl;
-    for (int i=0; i < Account::deposits.size(); i++) {
-        stream << "(" << i << ") " << Account::deposits[i] << std::endl;
-    }
-
-    stream << "--------------------------\nNumber of Withdrawals: " << Account::tW << "\n--------------------" << std::endl;
-    for (int i=0; i < Account::withdraws.size(); i++) {
-        stream << "(" << i << ") " << Account::withdraws[i] << std::endl;
+    stream << "--------------------------\nNumber of Withdrawals: " << acc.tW << "\n--------------------" << std::endl;
+    for (int i=0; i < acc.withdraws.size(); i++) {
+        stream << "(" << i << ") " << acc.withdraws[i] << std::endl;
     }
 
     return stream;
--- makefile
+++ makefile
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 program: main.cpp Money.o Account.o
 	g++ -std=c++11 -o program Money.o Account.o main.cpp
 
@@ -10,13 +9,3 @@ Account.o: Account.cpp Account.h
 
 clean:
 	rm *.o program
-=======
-pa3: main.o Money.o 
-	g++ -std=c++11 -o pa3 main.o Money.o
-main.o: main.cpp
-	g++ -std=c++11 -c main.cpp
-Money.o: Money.cpp Money.h
-	g++ -std=c++11 -c Money.cpp
-clean:
-	rm *.o pa3
->>>>>>> 4ebc7846a4c8f7208cdeefef35c70f23d9ce98b9
